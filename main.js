e518a101aa6eb0aa54eadf4813555483fdbf34c2
Bryan Castillo Marin <bryan_cm96@hotmail.com>
2026-02-20 00:21:33 -0600

    updating images and some descriptions in projects

--- main.js
+++ main.js
@@ -9,80 +9,68 @@ const projectsData = {
     "puentePeatonal": {
         title: "Puente Peatonal",
         service: "Descripción del servicio",
-        startDate: "01/01/2024",
+        startDate: "Enero 2025",
         endDate: "01/02/2024",
         location: "Ubicación del proyecto",
         description1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam temporibus debitis iste, inventore iusto neque repellat quidem deserunt sit! Aspernatur numquam perferendis rerum soluta ipsa voluptas inventore recusandae cumque quas.",
         description2: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est inventore tempora debitis totam consectetur aut numquam rerum molestiae, dolorem expedita soluta aliquid sunt ratione ad tempore, odit facilis delectus eos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius maiores, molestias culpa similique omnis aperiam explicabo atque vero dignissimos. Quo voluptas ipsam libero molestias, soluta quia? Facilis quia delectus nisi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed, accusamus accusantium ullam alias quos amet vitae, animi placeat eos necessitatibus, id ipsam veniam rem voluptatibus laborum iusto odio velit et!",
-        imageDescription: "Descripción de la imagen principal del proyecto",
-        imageUrl1: "./assets/img/puente_peatonal.JPG",
-        imageUrl2: "./assets/img/puente_peatonal.JPG",
-        imageUrl3: "./assets/img/puente_peatonal.JPG"
+        imageUrl1: "./assets/img/puente_peatonal_modal_1.jpg",
+        imageUrl2: "./assets/img/puente_peatonal_modal_2.JPG",
     },
     "hotelTulum": {
         title: "Hotel Tulum",
         service: "Descripción del servicio",
-        startDate: "01/01/2024",
+        startDate: "Julio 2024",
         endDate: "01/02/2024",
-        location: "Ubicación del proyecto",
+        location: "Zona hotelera Tulum",
         description1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam temporibus debitis iste, inventore iusto neque repellat quidem deserunt sit! Aspernatur numquam perferendis rerum soluta ipsa voluptas inventore recusandae cumque quas.",
         description2: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est inventore tempora debitis totam consectetur aut numquam rerum molestiae, dolorem expedita soluta aliquid sunt ratione ad tempore, odit facilis delectus eos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius maiores, molestias culpa similique omnis aperiam explicabo atque vero dignissimos. Quo voluptas ipsam libero molestias, soluta quia? Facilis quia delectus nisi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed, accusamus accusantium ullam alias quos amet vitae, animi placeat eos necessitatibus, id ipsam veniam rem voluptatibus laborum iusto odio velit et!",
-        imageDescription: "Descripción de la imagen principal del proyecto",
-        imageUrl1: "./assets/img/hotel_tulum.png",
-        imageUrl2: "./assets/img/hotel_tulum.png",
-        imageUrl3: "./assets/img/hotel_tulum.png"
+        imageUrl1: "./assets/img/hotel_tulum_modal_1.jpeg",
+        imageUrl2: "./assets/img/hotel_tulum_modal_2.jpeg",
     },
     "aeropuertoCancun": {
         title: "Aeropuerto Cancún",
         service: "Descripción del servicio",
-        startDate: "01/01/2024",
-        endDate: "01/02/2024",
-        location: "Ubicación del proyecto",
-        description1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam temporibus debitis iste, inventore iusto neque repellat quidem deserunt sit! Aspernatur numquam perferendis rerum soluta ipsa voluptas inventore recusandae cumque quas.",
-        description2: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est inventore tempora debitis totam consectetur aut numquam rerum molestiae, dolorem expedita soluta aliquid sunt ratione ad tempore, odit facilis delectus eos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius maiores, molestias culpa similique omnis aperiam explicabo atque vero dignissimos. Quo voluptas ipsam libero molestias, soluta quia? Facilis quia delectus nisi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed, accusamus accusantium ullam alias quos amet vitae, animi placeat eos necessitatibus, id ipsam veniam rem voluptatibus laborum iusto odio velit et!",
-        imageDescription: "Descripción de la imagen principal del proyecto",
-        imageUrl1: "./assets/img/aeropuerto_cancun.JPG",
-        imageUrl2: "./assets/img/aeropuerto_cancun.JPG",
-        imageUrl3: "./assets/img/aeropuerto_cancun.JPG"
+        startDate: "Octubre 2024",
+        endDate: "Marzo 2025",
+        location: "Cancún, Quintana Roo",
+        description1: "Nuestra intervención se centró en la fabricación de elementos estructurales metálicos diseñados para soportar altos flujos de operación.",
+        description2: "Gracias al dominio de procesos multitécnicos (FCAW, GMAW y SMAW), logramos optimizar los tiempos de entrega sin comprometer la precisión técnica requerida por la dirección de obra. ",
+        imageUrl1: "./assets/img/aeropuerto_cancun_modal_1.jpg",
+        imageUrl2: "./assets/img/aeropuerto_cancun_modal_2.jpg",
     },
     "trenMaya": {
         title: "Tren Maya",
         service: "Descripción del servicio",
-        startDate: "01/01/2024",
+        startDate: "Diciembre 2023",
         endDate: "01/02/2024",
-        location: "Ubicación del proyecto",
+        location: "Quintana Roo",
         description1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam temporibus debitis iste, inventore iusto neque repellat quidem deserunt sit! Aspernatur numquam perferendis rerum soluta ipsa voluptas inventore recusandae cumque quas.",
         description2: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est inventore tempora debitis totam consectetur aut numquam rerum molestiae, dolorem expedita soluta aliquid sunt ratione ad tempore, odit facilis delectus eos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius maiores, molestias culpa similique omnis aperiam explicabo atque vero dignissimos. Quo voluptas ipsam libero molestias, soluta quia? Facilis quia delectus nisi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed, accusamus accusantium ullam alias quos amet vitae, animi placeat eos necessitatibus, id ipsam veniam rem voluptatibus laborum iusto odio velit et!",
-        imageDescription: "Descripción de la imagen principal del proyecto",
-        imageUrl1: "./assets/img/tren_maya.jpg",
-        imageUrl2: "./assets/img/tren_maya.jpg",
-        imageUrl3: "./assets/img/tren_maya.jpg"
+        imageUrl1: "./assets/img/tren_maya_modal_1.jpg",
+        imageUrl2: "./assets/img/tren_maya_modal_2.jpg"
     },
     "domosChetumal": {
         title: "Domos Chetumal",
         service: "Descripción del servicio",
         startDate: "01/01/2024",
         endDate: "01/02/2024",
-        location: "Ubicación del proyecto",
+        location: "Chetumal",
         description1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam temporibus debitis iste, inventore iusto neque repellat quidem deserunt sit! Aspernatur numquam perferendis rerum soluta ipsa voluptas inventore recusandae cumque quas.",
         description2: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est inventore tempora debitis totam consectetur aut numquam rerum molestiae, dolorem expedita soluta aliquid sunt ratione ad tempore, odit facilis delectus eos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius maiores, molestias culpa similique omnis aperiam explicabo atque vero dignissimos. Quo voluptas ipsam libero molestias, soluta quia? Facilis quia delectus nisi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed, accusamus accusantium ullam alias quos amet vitae, animi placeat eos necessitatibus, id ipsam veniam rem voluptatibus laborum iusto odio velit et!",
-        imageDescription: "Descripción de la imagen principal del proyecto",
-        imageUrl1: "./assets/img/domos_chetumal.jpg",
-        imageUrl2: "./assets/img/domos_chetumal.jpg",
-        imageUrl3: "./assets/img/domos_chetumal.jpg"
+        imageUrl1: "./assets/img/domos_chetumal_modal_1.jpg",
+        imageUrl2: "./assets/img/domos_chetumal_modal_2.jpg"
     },
     "refineriaOlmeda": {
         title: "Refinería Olmeda",
         service: "Descripción del servicio",
-        startDate: "01/01/2024",
-        endDate: "01/02/2024",
-        location: "Ubicación del proyecto",
+        startDate: "Marzo 2022",
+        endDate: "Julio 2024",
+        location: "Paraíso, Tabasco",
         description1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam temporibus debitis iste, inventore iusto neque repellat quidem deserunt sit! Aspernatur numquam perferendis rerum soluta ipsa voluptas inventore recusandae cumque quas.",
         description2: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est inventore tempora debitis totam consectetur aut numquam rerum molestiae, dolorem expedita soluta aliquid sunt ratione ad tempore, odit facilis delectus eos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius maiores, molestias culpa similique omnis aperiam explicabo atque vero dignissimos. Quo voluptas ipsam libero molestias, soluta quia? Facilis quia delectus nisi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed, accusamus accusantium ullam alias quos amet vitae, animi placeat eos necessitatibus, id ipsam veniam rem voluptatibus laborum iusto odio velit et!",
-        imageDescription: "Descripción de la imagen principal del proyecto",
-        imageUrl1: "./assets/img/puente_peatonal.jpg",
-        imageUrl2: "./assets/img/puente_peatonal.jpg",
-        imageUrl3: "./assets/img/puente_peatonal.jpg"
+        imageUrl1: "./assets/img/refineria_olmeda_modal_1.jpg",
+        imageUrl2: "./assets/img/refineria_olmeda_modal_2.jpg"
     }
 }
 
@@ -96,10 +84,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const modalStartDate = document.getElementById("modalStartDate");
     const modalEndDate = document.getElementById("modalEndDate");
     const modalLocation = document.getElementById("modalLocation");
-    const modalImageDescription = document.getElementById("modalImageDescription");
     const modalImage1 = document.getElementById("modalImage1");
     const modalImage2 = document.getElementById("modalImage2");
-    const modalImage3 = document.getElementById("modalImage3");
     const menuButton = document.getElementById("menuButton");
     const menuButtonClose = document.getElementById("menuButtonClose");
     const menu = document.getElementById("menu");
@@ -140,10 +126,8 @@ document.addEventListener('DOMContentLoaded', function() {
         modalLocation.textContent = project.location;
         modalDescription1.textContent = project.description1;
         modalDescription2.textContent = project.description2;
-        modalImageDescription.textContent = project.imageDescription;
         modalImage1.src = project.imageUrl1;
         modalImage2.src = project.imageUrl2;
-        modalImage3.src = project.imageUrl3;
     }
 
     modal.addEventListener("click", function(event) {
